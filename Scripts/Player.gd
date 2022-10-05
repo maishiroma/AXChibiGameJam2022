c26becdab49574d8231b5130ce2641f32582bd5e
Matthew Shiroma <19522250+maishiroma@users.noreply.github.com>
2022-10-04 21:58:51 -0700

    Added dummy platform, more player controls

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,23 +1,45 @@
 # This allows for movement by force
 extends KinematicBody2D
 
-export var move_speed = 10
-export var jump_speed = 10
+# What direction is the player going UP
+const UP_DIRECTION = Vector2.UP
 
+# Variables that are exposed to the inspector
+export var move_speed = 2000.0
+export var jump_strength = 2000.0
+export var gravity_scale = 4500.0
+
+# Private variables
 var move_velocity = Vector2.ZERO
+
+var is_falling = false	# check if we are grounded
+var is_jumping = false # check if we are jumping
+var is_jump_canceled = false # check if we cancelled the jump so we can have more control
 
 # This is equivalent to FixedUpdate
 func _physics_process(delta):
+	
+	# Get player inputs
 	get_input()
 	
-	# Allows us to use a built in method to control movement speed
-	var motion = move_velocity * delta
-	move_and_collide(motion)
+	## Gravity
+	move_velocity.y += gravity_scale * delta
+	
+	## Checks for specific flags
+	is_falling = move_velocity.y >= 0.0 and not is_on_floor()
+	
+	## Jumping
+	if is_jumping:
+		move_velocity.y = -jump_strength
+	elif is_jump_canceled:
+		move_velocity.y = 0.0
+	
+	move_velocity = move_and_slide(move_velocity, UP_DIRECTION)
 
+# Take player input and modify the move_velocity accordingly
 func get_input():
-	if Input.is_action_pressed("move_left"):
-		move_velocity.x += -1
-	elif Input.is_action_pressed("move_right"):
-		move_velocity.x += 1
-	else:
-		move_velocity.x = 0
+	is_jumping = Input.is_action_just_pressed("Jump") and is_on_floor()
+	is_jump_canceled = Input.is_action_just_released("Jump") and move_velocity.y < 0.0
+	
+	var horizontal_direction = (Input.get_action_strength("move_right") - Input.get_action_strength("move_left"))
+	move_velocity.x = horizontal_direction * move_speed
