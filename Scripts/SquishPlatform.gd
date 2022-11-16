ce92cc3b96a7b8684130c893ff627e17387edc31
Matthew Shiroma <19522250+maishiroma@users.noreply.github.com>
2022-11-15 22:08:24 -0800

    Adding new feature to reset platform

--- Scripts/SquishPlatform.gd
+++ Scripts/SquishPlatform.gd
@@ -6,18 +6,25 @@ const UP_DIRECTION = Vector2.UP
 # Public variables to determine block behavior
 export var move_rate = 2
 export var move_vertical = true
+export var reset_position = false
 export var bounce_strength = 100.0
+
+var original_location = Vector2.ZERO
 
 # Private Variables
 var move_velocity = Vector2.ZERO
 
 func _ready():
 	$AnimatedSprite.animation = "idle"
+	original_location = position
 
 # Determines block movement
 func _physics_process(_delta):
-	# warning-ignore:return_value_discarded
-	move_and_collide(move_velocity, false)
+	if reset_position:
+		print("IsMoving")
+	else:
+		# warning-ignore:return_value_discarded
+		move_and_collide(move_velocity, false)
 
 # When the player hits this, the block moves
 func _on_Player_hammer_hit(collider_name):
@@ -36,3 +43,10 @@ func _on_MoveTime_timeout():
 	move_velocity = Vector2.ZERO
 	$AnimatedSprite.animation = "idle"
 	$AnimatedSprite.stop()
+	$ResetTime.start()
+
+
+# This tells the platform to move back
+func _on_ResetTime_timeout():
+	print("Time Reset")
+	pass
