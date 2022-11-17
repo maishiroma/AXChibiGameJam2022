990cb3cc5dee8722cfaeb9a8ff86990be70c11ec
Matthew Shiroma <19522250+maishiroma@users.noreply.github.com>
2022-11-16 21:40:07 -0800

    Finished coding new restart positon for move blocks

--- Scripts/SquishPlatform.gd
+++ Scripts/SquishPlatform.gd
@@ -7,12 +7,13 @@ const UP_DIRECTION = Vector2.UP
 export var move_rate = 2
 export var move_vertical = true
 export var reset_position = false
+export var time_to_reset = 0.5
 export var bounce_strength = 100.0
-
-var original_location = Vector2.ZERO
 
 # Private Variables
 var move_velocity = Vector2.ZERO
+var original_location = Vector2.ZERO
+var is_returning = false
 
 func _ready():
 	$AnimatedSprite.animation = "idle"
@@ -20,22 +21,37 @@ func _ready():
 
 # Determines block movement
 func _physics_process(_delta):
-	if reset_position:
-		print("IsMoving")
+	if reset_position and is_returning:
+		# If we are resetting the block's position we do it gradually
+		var result = position - original_location
+		if int(result.length_squared()) != 0:
+			determine_movement(move_rate * -1)
+			position += move_velocity
+		else:
+			position = original_location
+			move_velocity = Vector2.ZERO
+			is_returning = false
 	else:
 		# warning-ignore:return_value_discarded
 		move_and_collide(move_velocity, false)
 
+
+func determine_movement(speed):
+	if move_vertical == false:
+		move_velocity = Vector2(1 * speed, 0)
+	else:
+		move_velocity = Vector2(0, 1 * speed)
+
 # When the player hits this, the block moves
 func _on_Player_hammer_hit(collider_name):
 	if name == collider_name:
-		if move_vertical == false:
-			move_velocity = Vector2(1 * move_rate, 0)
-		else:
-			move_velocity = Vector2(0, 1 * move_rate)
+		determine_movement(move_rate)
 		$AnimatedSprite.animation = "move"
 		$AnimatedSprite.speed_scale = 5
 		$AnimatedSprite.play()
+		if $ResetTime.time_left > 0:
+			is_returning = false
+			$ResetTime.stop()
 		$MoveTime.start()
 
 # Upon a set timer, the block stops moving
@@ -43,10 +59,8 @@ func _on_MoveTime_timeout():
 	move_velocity = Vector2.ZERO
 	$AnimatedSprite.animation = "idle"
 	$AnimatedSprite.stop()
-	$ResetTime.start()
-
+	$ResetTime.start(time_to_reset)
 
 # This tells the platform to move back
 func _on_ResetTime_timeout():
-	print("Time Reset")
-	pass
+	is_returning = true
